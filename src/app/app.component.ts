a809408c88c568829068419a3c7f113bce91f117
denys.bykanov <denys.bykanov@dev-pro.net>
2019-03-15 18:13:59 +0200

    feature: data from component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TodoItem} from './models/todoItem.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'myTODO';
+
+
+export class AppComponent implements OnInit {
+  public todoItemList: TodoItem[] = [];
+
+  ngOnInit(): void {
+    this.todoItemList = [
+      {
+        id: 1,
+        text: 'Learn CSS',
+        isDone: false
+      }, {
+        id: 2,
+        text: 'Learn JS',
+        isDone: false
+      }, {
+        id: 3,
+        text: 'Learn HTML',
+        isDone: false
+      }, {
+        id: 4,
+        text: 'If you wont to get smth - do smth',
+        isDone: true
+      }, {
+        id: 5,
+        text: 'Integrate Terminal contract from XDC to POS Application',
+        isDone: false
+      }, {
+        id: 6,
+        text: 'Drawer sessions refactoring',
+        isDone: true
+      },
+    ];
+  }
+
 }
