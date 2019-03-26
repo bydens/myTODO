37cd1a5c29275c925679b92173b3102613506f10
Denys Bykanov <bydens@gmail.com>
2019-03-26 21:08:03 +0200

    feature: store dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import {StoreModule} from '@ngrx/store';
 import {AppComponent} from './app.component';
 import {TodoModule} from './todo/todo.module';
 
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     TodoModule,
     BrowserModule,
-    StoreModule.forRoot( {})
+    StoreModule.forRoot( {}),
+    StoreDevtoolsModule.instrument({
+      name: 'APM Demo App DevTools',
+      maxAge: 25,
+      logOnly: environment.production,
+    })
   ],
   bootstrap: [AppComponent]
 })
